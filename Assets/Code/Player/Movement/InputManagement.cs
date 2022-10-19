bc90669cd30842f9e7585cccfdbd5c3bb91ec926
vinivss <vinivss@hotmail.com>
2022-10-18 21:37:25 -0700

    Jumping and wall Running v1.0 up

--- Assets/Code/Player/Movement/InputManagement.cs
+++ Assets/Code/Player/Movement/InputManagement.cs
@@ -1,15 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-
+using UnityEditor;
 using UnityEngine;
 
 public class InputManagement : MonoBehaviour
 {
     [Header("Movement Values")]
-    [Tooltip("basic movement Speed")]public float movespeed = 6f;
-    [Tooltip("How strong is the Jump")] public float jumpStrength = 10f;
-    [Tooltip("How many times can you Jump")] public int maxJumps = 1;
+    [Tooltip("basic movement Speed")]
+    public float movespeed = 6f;
+    float movementMultiplier = 10f;
+    [Tooltip("How strong is the Jump")]
+    public float jumpStrength = 10f;
+    [Tooltip("How many times can you Jump")]
+    public int maxJumps = 1;
+    [Tooltip("How fast you move while in the air")]
+    [SerializeField] float airMultiplier = 10f;
 
     [Header("Camera")]
     [SerializeField] Camera cam;
@@ -18,15 +24,26 @@ public class InputManagement : MonoBehaviour
     [SerializeField] float groundDrag = 6f;
     [SerializeField] float airDrag = 3f;
 
+    [Header("Floor detection")]
+    [Tooltip("Center of Sphere for ground Detection")]
+    [SerializeField] Transform groundCheckPos;
+    [Min(0)][Tooltip("Radius of Sphere used in ground detection")]
+    [SerializeField] float groundDistance = 0.4f;
+    [Tooltip("layer for ground to be detected")]
+    [SerializeField] LayerMask groundMask;
+
     FPSControls inputs;
     Vector2 currentMove;
     Vector2 camDelta;
     float playerHeight = 2f;
-    bool jumpPerform;
+    [HideInInspector]public bool jumpPerform;
     bool isGrounded;
     int jumps = 0;
 
     Vector3 moveDir;
+    Vector3 slopeMoveDir;
+
+    RaycastHit slopeHit;
 
     Rigidbody Rb;
     private void Awake()
@@ -37,7 +54,7 @@ public class InputManagement : MonoBehaviour
         inputs.Player.Movement.performed += ctx =>
         {
             currentMove = ctx.ReadValue<Vector2>();
-            Debug.Log(currentMove);
+           // Debug.Log(currentMove);
         };
 
         inputs.Player.Movement.canceled += ctx =>
@@ -60,11 +77,19 @@ public class InputManagement : MonoBehaviour
         };
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheckPos.position, groundDistance);
+    }
     private void Update()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight / 2 + 0.1f);
+
+        isGrounded = Physics.CheckSphere(groundCheckPos.position, groundDistance, groundMask);
 
         ControlDrag();
+
+        slopeMoveDir = Vector3.ProjectOnPlane(moveDir, slopeHit.normal);
     }
 
 
@@ -87,6 +112,7 @@ public class InputManagement : MonoBehaviour
     {
         if (isGrounded)
         {
+            Rb.velocity = new(Rb.velocity.x, 0, Rb.velocity.z);
             Rb.drag = groundDrag;
         }
 
@@ -95,16 +121,42 @@ public class InputManagement : MonoBehaviour
             Rb.drag = airDrag;
         }
     }
+
+    private bool OnSlope()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
+        {
+            if(slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
     private void CharacterJump()
     {
-        Debug.Log("JUmpu desu");
+       // Debug.Log("JUmpu desu");
         Rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
     }
 
     private void MoveCharacter()
     {
         moveDir = cam.transform.forward * currentMove.y + cam.transform.right * currentMove.x;
-        Rb.AddForce(moveDir.normalized * movespeed, ForceMode.Acceleration);
+        moveDir.y = 0;
+        if (isGrounded && !OnSlope())
+        {
+            Rb.AddForce(moveDir.normalized * movespeed * movementMultiplier, ForceMode.Acceleration);
+        }
+
+        else if (isGrounded && OnSlope())
+        {
+            Rb.AddForce(slopeMoveDir.normalized * movespeed * movementMultiplier, ForceMode.Acceleration);
+        }
+
+        else if(!isGrounded)
+        {
+            Rb.AddForce(moveDir.normalized * movespeed * airMultiplier * movementMultiplier, ForceMode.Acceleration);
+        }
     }
 
     private void OnEnable()
---
+++ Assets/Code/Player/Movement/WallRun.cs
@@ -0,0 +1,130 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class WallRun : MonoBehaviour
+{
+    [Header("Movement Reference")]
+    [SerializeField] Transform orientation;
+
+    [Header("Detection")]
+    [Tooltip("How far away from the wall do you need to be")]
+    [SerializeField] float wallDistance = 0.5f;
+    [Tooltip("Minimum jump height before attaching to wall")]
+    [SerializeField] float minimumJumpHeight = 1.5f;
+    [Tooltip("layers to be checked for wall collision")]
+    [SerializeField] LayerMask WallChecks;
+
+    [Header("Movement")]
+    [Tooltip("How fast do you slide down a wall")]
+    [SerializeField] float wallGravity;
+    [Tooltip("How Strong the Wall jump is")]
+    [SerializeField] float wallJumpStrength;
+
+    [Header("Camera")]
+    [Tooltip("Camera to apply Effects to")]
+    [SerializeField] CinemachineVirtualCamera cam;
+    [Tooltip("How much to Dutch Angle")]
+    [SerializeField] float dutchAngle = 4.0f;
+
+    bool wallLeft = false;
+    bool wallRight = false;
+
+    RaycastHit leftWallHit;
+    RaycastHit rightWallHit;
+
+    Rigidbody rb;
+
+    InputManagement input;
+
+
+    private void Start()
+    {
+        input = GetComponent<InputManagement>();   
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        CheckWall();
+        if(CanWallRun())
+        {
+            if(wallLeft)
+            {
+                StartWallRun();
+                Debug.Log("On Your Left");
+            } 
+            if (wallRight)
+            {
+                StartWallRun();
+                Debug.Log("On Your Right");
+            }
+
+            else
+            {
+                EndWallRun();
+            }
+        }
+        else
+        {
+            EndWallRun();
+        }
+    }
+
+    void CheckWall()
+    {
+        Debug.DrawRay(transform.position, -orientation.right * wallDistance, Color.green) ;
+        wallLeft = Physics.Raycast(transform.position, -orientation.right,out leftWallHit, wallDistance, WallChecks );
+
+        Debug.DrawRay(transform.position, orientation.right * wallDistance, Color.green);
+        wallRight = Physics.Raycast(transform.position, orientation.right,out rightWallHit, wallDistance, WallChecks);
+    }
+
+    bool CanWallRun()
+    {
+        return !Physics.Raycast(transform.position, Vector3.down, minimumJumpHeight);
+    }
+
+    void StartWallRun()
+    {
+        rb.useGravity = false;
+        rb.AddForce(Vector3.down * wallGravity, ForceMode.Force);
+        if(wallLeft)
+        {
+            cam.m_Lens.Dutch = -dutchAngle;
+        }
+
+        else if(wallRight)
+        {
+            cam.m_Lens.Dutch = dutchAngle;
+        }
+        if (input.jumpPerform)
+        {
+            if (wallLeft)
+            { 
+                Vector3 wallJumpDir = transform.up + leftWallHit.normal;
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+                rb.AddForce(wallJumpDir * wallJumpStrength * 100, ForceMode.Force);
+                cam.m_Lens.Dutch = 0.0f;
+            }
+
+            else if (wallRight)
+            {
+                Vector3 wallJumpDir = transform.up + rightWallHit.normal;
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+                rb.AddForce(wallJumpDir * wallJumpStrength * 100, ForceMode.Force);
+                cam.m_Lens.Dutch = 0.0f;
+            }
+        }
+    }
+
+    void EndWallRun()
+    {
+        rb.useGravity = true;
+        cam.m_Lens.Dutch = 0.0f;
+
+    }
+}
